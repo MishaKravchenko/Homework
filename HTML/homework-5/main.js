54332323664634221d928dcfd4aba08e2b90f5c3
MishaKravchenko <mishakravchenko.1998@gmail.com>
2020-10-29 13:30:05 +0200

    homework-5

---
+++ HTML/homework-5/main.js
@@ -0,0 +1,417 @@
+//1-2 завдання
+// let helloWorld = 'Kravchenko';
+// console.log(helloWorld);
+// alert(helloWorld);
+// document.write(helloWorld)
+//
+// let bestCourses = 'Mykhailo';
+// console.log(bestCourses);
+// alert(bestCourses);
+// document.write(bestCourses);
+//
+// let ukrPravda = 'Ivanovich';
+// console.log(ukrPravda);
+// alert(ukrPravda);
+// document.write(ukrPravda);
+//
+// let com = 'Lviv';
+// console.log(com);
+// alert(com);
+// document.write(com);
+//
+// let number = 9;
+// console.log(number);
+// alert(number);
+// document.write(number);
+//
+// let nineOr = 10;
+// console.log(nineOr);
+// alert(nineOr);
+// document.write(nineOr);
+//
+// let troll = 999;
+// console.log(troll);
+// alert(troll);
+// document.write(troll);
+//
+// let abc = 321;
+// console.log(abc);
+// alert(abc);
+// document.write(abc);
+//
+// let numberPi = 4.13;
+// console.log(numberPi);
+// alert(numberPi);
+// document.write(numberPi);
+//
+// let what = 21;
+// console.log(what);
+// alert(what);
+// document.write(what);
+//
+// let square = 4;
+// console.log(square);
+// alert(square);
+// document.write(square);
+//
+// let question = false;
+// console.log(question);
+// alert(question);
+// document.write(question);
+//
+// let question2 = true;
+// console.log(question2);
+// alert(question2);
+// document.write(question2);
+
+//3 завдання
+// const country = 'Ukraine'
+// const city = 'Lviv'
+// const street = 'Lazarenko'
+//
+// console.log(country);
+// console.log(city);
+// console.log(street);
+//
+// alert(country);
+// alert(city);
+// alert(street);
+//
+// document.write(country);
+// document.write(city);
+// document.write(street);
+//
+// const numberMessi = 10;
+// const numberRonaldo = 7;
+// const numberNeymarJr = 11;
+//
+// console.log(numberMessi);
+// console.log(numberRonaldo);
+// console.log(street);
+//
+// alert(numberMessi);
+// alert(numberRonaldo);
+// alert(numberNeymarJr);
+//
+// document.write(numberMessi);
+// document.write(numberRonaldo);
+// document.write(numberNeymarJr);
+
+//4,5,6 завдання
+// prompt('Прізвище', 'Кравченко');
+// prompt('Ім\'я','Михайло');
+// prompt('По батькові', 'Іванович');
+//
+// let surName1 = 'Kravchenko';
+// let name1 = 'Mykhailo';
+// let middleName1 = 'Ivanovich';
+//
+// console.log(surName1);
+// console.log(name1);
+// console.log(middleName1);
+//
+// alert(surName1);
+// alert(name1);
+// alert(middleName1);
+//
+// document.write(surName1);
+// document.write(name1);
+// document.write(middleName1);
+//
+// let person1= surName1+name1+middleName1;
+// console.log(person1);
+//
+// prompt('Прізвище', 'Кравченко');
+// prompt('Ім\'я','Іван');
+// prompt('По батькові', 'Михайлович');
+//
+// let surName2 = 'Kravchenko';
+// let name2 = 'Ivan';
+// let middleName2 = 'Mykhailovych';
+//
+// console.log(surName2);
+// console.log(name2);
+// console.log(middleName2);
+//
+// alert(surName2);
+// alert(name2);
+// alert(middleName2);
+//
+// document.write(surName2);
+// document.write(name2);
+// document.write(middleName2);
+//
+// let person2= surName2+name2+middleName2;
+// console.log(person2);
+//
+// prompt('Прізвище', 'Кравченко');
+// prompt('Ім\'я','Лілія');
+// prompt('По батькові', 'Василівна');
+//
+// let surName3 = 'Kravchenko';
+// let name3 = 'Liliya';
+// let middleName3 = 'Vasilivna';
+//
+// console.log(surName3);
+// console.log(name3);
+// console.log(middleName3);
+//
+// alert(surName3);
+// alert(name3);
+// alert(middleName3);
+//
+// document.write(surName3);
+// document.write(name3);
+// document.write(middleName3);
+//
+// let person3= surName3+name3+middleName3;
+// console.log(person3);
+//
+// prompt('Прізвище', 'Кравченко');
+// prompt('Ім\'я','Анна');
+// prompt('По батькові', 'Іванівна');
+//
+// let surName4 = 'Kravchenko';
+// let name4 = 'Anna';
+// let middleName4 = 'Ivanivna';
+//
+// console.log(surName4);
+// console.log(name4);
+// console.log(middleName4);
+//
+// alert(surName4);
+// alert(name4);
+// alert(middleName4);
+//
+// document.write(surName4);
+// document.write(name4);
+// document.write(middleName4);
+//
+// let person4= surName4+name4+middleName4;
+// console.log(person4);
+
+//7 завдання
+// let number1 = +prompt('Введіть число');
+// let number2 = +prompt('Введіть число');
+// let number3 = +prompt('Введіть число');
+//
+// console.log(number1);
+// console.log(number2);
+// console.log(number3);
+
+//8 завдання
+// let number4 = parseInt(prompt('Введіть число'));
+// let number5 = parseInt(prompt('Введіть число'));
+// let number6 = parseInt(prompt('Введіть число'));
+// let number7 = parseInt(prompt('Введіть число'));
+//
+// let result= number4+number5+number6+number7;
+// console.log(result);
+
+//9 завдання
+// let number8 =prompt('Введіть Float число');
+// let number9 =prompt('Введіть Float число');
+// let number10 =prompt('Введіть Float число');
+//
+// let result= (parseFloat(number8))+(parseFloat(number9))+(parseFloat(number10));
+// console.log(result);
+
+//10 завдання
+// let number11 =prompt('Введіть Float число');
+// let number12 =prompt('Введіть Float число');
+// let number13 =prompt('Введіть Float число');
+//
+// let result= (Math.round(number11))+(Math.round(number12))+(Math.round(number13));
+// console.log(result);
+
+//11 завдання
+// let num1 = prompt('Введіть ціле число');
+// let num2 = prompt('Введіть ціле число');
+//
+// let result= Math.pow(num1 ,num2);
+// console.log(result)
+
+//12 завдання
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+// console.log(typeof a);
+// console.log(typeof b);
+// console.log(typeof c);
+// console.log(typeof d);
+
+//13 завдання
+//
+// console.log(5<6);
+// console.log(5>6);
+// console.log(5>=6);
+// console.log(5===6);
+//
+// console.log(10>=10);
+// console.log(10===10);
+// console.log(10!==10);
+// console.log(10>10);
+// console.log(10<10);
+//
+// console.log(123==='123');
+// console.log(123!=='123');
+
+//Додаткове завдання
+// (true && true) => true;
+// (false && true) => false;
+// (true || false) => true;
+// (true || true) => true;
+// ((true || true) && (true || false)) => (true && true) => true;
+// ((true || true)||(true || false)) => (true || true) => true;
+// ((true && false)) && (true || false) => (false && true) => false;
+// ((true || true) || (false && true)) => (true || false) => true;
+// true;
+// true;
+// true;
+// true;
+// true;
+// false;
+// ((true || true) || true) => true;
+// (true || true) && (false && true) => false;
+
+
+// _______________________________________________
+// CLASS
+// 1 завдання.
+// let str = 'Привет';
+// console.log(str);
+// let num = 123;
+// console.log(num);
+// let flag = true;
+// console.log(flag);
+// let txt = 'true';
+// console.log(txt);
+// console.log(typeof str);
+// console.log(typeof num);
+// console.log(typeof flag);
+// console.log(typeof txt);
+
+// _______________________________________________
+// 2 завдання.
+// let a1 = 5 + 3;
+// console.log(a1);
+// let a2 = 5 - 3;
+// console.log(a2);
+// let a3 = 5 * 3;
+// console.log(a3);
+// let a4 = 5 / 3;
+// console.log(a4);
+// let a5 = 5 % 3;
+// console.log(a5);
+
+
+// _______________________________________________
+// 3 завдання.
+// let a6 = 5 % 3;
+// console.log(a6);
+// let a7 = 3 % 5;
+// console.log(a7);
+// let a8 = 5 + '3';
+// console.log(a8);
+// let a9 = '5' - 3;
+// console.log(a9);
+// let a10 = 75 + 'кг';
+// console.log(a10);
+
+
+// _______________________________________________
+// 4 завдання.
+// let height = 23;
+// let width =10;
+// let s = (height*width);
+// console.log(s);
+
+// _______________________________________________
+// 5 завдання.
+// let heightC = 10;
+// let dC = 4;
+// let numberPi = 3.14;
+// let rC = (dC/2);
+// console.log(rC);
+//
+// // let v = ((numberPi*rC*rC)*heightC);
+// // console.log(v);
+//
+// let v= ((numberPi*rC**2)*heightC);
+// console.log(v);
+
+// _______________________________________________
+// 6 завдання.
+// let n = 3;
+// let m = 4;
+// let k1 = (Math.pow(3,2)) + (Math.pow(4,2));
+// let k = Math.sqrt(k1);
+// console.log(k);
+
+// _______________________________________________
+// 7 завдання.
+// let str = 'Hello World'
+// console.log(str);
+// alert(str);
+// document.write(str);
+
+// _______________________________________________
+// 8 завдання.
+// let surName ='Kravchenko';
+// let name ='Mykhailo';
+// let middleName ='Ivanovich';
+// let age = 21;
+// let hobbie = 'sleep';
+//
+// // let me = surName+'\n' + name+'\n' + middleName+'\n'+ age+'\n' + hobbie+'\n';
+// let me = `${surName}\n${name}\n${middleName}\n${age}\n${hobbie}`
+//
+// alert(me);
+
+
+// _______________________________________________
+// 9 завдання.
+// let str1 = 'Хто';
+// let str2 = 'ти';
+// let str3 = 'такий?';
+// let concatenation = `${str1}\n${str2}\n${str3}`;
+// document.write(concatenation);
+
+
+// _______________________________________________
+// 10 завдання.
+// 205 - js не виконує дію додавання, а виконує контактинацію;
+// 15 - виконується віднімання;
+// 40 - виконується множення;
+// 10 -виконується дідення;
+// <br/> - переносе рядок на новий абзац
+
+// _______________________________________________
+// 11 завдання.
+// parseInt("3.14") (3)
+// parseInt("-7.875") (-7)
+// parseInt("435") (435)
+// parseInt("Вася") (NaN)
+
+// _______________________________________________
+// 12 завдання.
+// let str = prompt("Введіть щось")
+// console.log(str);
+
+// _______________________________________________
+// 13 завдання.
+// let a1 = prompt('Введіть перше число');
+// let a2 = prompt('Введіть друге число');
+// let a3 = Number(a1)+Number(a2);
+// alert(a3);
+
+// _______________________________________________
+// 14 завдання.
+//
+// let name = prompt('Введіть імя');
+// let surName = prompt('Введіть прізвище');
+// let age = prompt('Введіть свій вік');
+//
+// alert(`${'Добрий день,'}\n${name}\n${surName}\n${'мої вітання - Вам'}\n${age}`);
+
